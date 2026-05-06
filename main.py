9a656c0114d75b93c6a61365cf8c8f57a7b99053
martfiddler <martfiddler@gmail.com>
2026-05-06 21:25:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from fastapi import FastAPI
 from routes_auth import router
 
 app = FastAPI()
-
+@app.get("/")
+def root():
+  return {"message": "Cybesure API is running"}
 app.include_router(router)
