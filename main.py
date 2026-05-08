f2f80d0b0ec67dc9bcb6e0d8a875e4699481e728
martfiddler <martfiddler@gmail.com>
2026-05-08 17:28:28 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes_auth import router
-
-app = FastAPI()
-@app.get("/")
-def root():
-  return {"message": "Cybesure API is running"}
-app.include_router(router)
